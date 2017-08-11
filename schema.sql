3bd60115c6126486fd14ff3980636f7d5b2393c9
Mac User <sabbey37@gmail.com>
2017-08-11 17:44:40 -0400

    Start creating schema

---
+++ schema.sql
@@ -0,0 +1,17 @@
+create table users (
+    user_id serial primary key,
+    name varchar (200) not null,
+    email varchar (200) not null,
+    password varchar (50) not null,
+    team_id integer references teams
+);
+
+create table teams (
+    
+);
+
+create table events (
+    event_id serial primary key,
+    event_date timestamp not null,
+    team_id integer references teams
+);
