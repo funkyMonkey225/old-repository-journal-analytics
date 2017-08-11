55de3f07c231c0508295ad3fcfca8b03d4d5da13
Katie <katherinelane11@gmail.com>
2017-08-11 11:56:45 -0400

    adds event plus with listener

---
+++ public/resources/scripts/main.js
@@ -0,0 +1,17 @@
+const $ADD_EVENT = $('[data-type="add-event"]');
+
+
+
+// function addEventListener() {
+//     $ADD_EVENT.click(function (){
+//         console.log("listening");
+//     });
+// };
+
+
+
+// // //Initialization
+// $(document).ready(function (){
+//     addEventListener();
+
+// });
