2824b80e21b002372fa7e9a144a2daac95b7cbae
Katie <katherinelane11@gmail.com>
2017-08-11 17:14:50 -0400

    fixes form popup

--- public/resources/scripts/main.js
+++ public/resources/scripts/main.js
@@ -1,15 +1,34 @@
-const $ADD_EVENT = $('[data-type="add-event"]');
+const $ADD_EVENT = $('[data-type="open-event-popup"]');
+const $FORM_CONTAINER = $('[data-popup="form-container"]');
+const $POPUP_BOX =  $('[data-popup="event-plus-popup"]');
+const $CLOSE_POPUP = $('[data-popup="close-event-popup"]');
 
-
-
-function plusSignListener() {
-    $ADD_EVENT.click(function (){
+// event listeners for adding events and closing event popup
+const plusSignButton = () => {
+    $ADD_EVENT.click((event) => {
+        event.preventDefault();
+        openEventAddForm();
         console.log("listening");
     });
+};
+const closePopupButton = () => {
+    $CLOSE_POPUP.click((event) => {
+        event.preventDefault();
+        closeEventAddForm();
+        console.log("closing");
+    });
+};
+const openEventAddForm = () => {
+    $FORM_CONTAINER.show('slow');
+};
+const closeEventAddForm = () => {
+    $FORM_CONTAINER.hide('slow');
 };
 
 
 
-// //Initialization
-plusSignListener();
+////Initialization
+$FORM_CONTAINER.hide();
+closePopupButton();
+plusSignButton();
 
