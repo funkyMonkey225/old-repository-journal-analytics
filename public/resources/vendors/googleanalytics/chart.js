c0ff2ca4f6ad63c0f079d6410b1a4405cd9872f3
gnohj21 <gnohj21@gmail.com>
2017-08-13 23:47:16 -0400

    added api docs, listeners for query/rendering, can retrieve user info now as well as extract raw data

--- public/resources/vendors/googleanalytics/chart.js
+++ public/resources/vendors/googleanalytics/chart.js
@@ -1,4 +1,17 @@
+/******************************************************************  
+ Google Analytic main Methods:
+    static method - gapi.analytics.ready()
+    static method - gapi.analytics.auth.authorize()
+    constructor - gapi.analytics.googleCharts.Datachart()
+    constructor - gapi.analytics.ext.DateRangeSelector()
+    constructor - gapi.analytics.ext.ViewSelector2()
 
+
+ Google Analytic extended Methods:
+    set() - Sets or updates the component's configuration options (this can also be done at creation time in the constructor).
+    execute() - Invokes the component's primary action. This is usually rendering something on the page or running a report (or both).
+    get() - Returns the current configuration options of a component.
+*******************************************************************/
 
 (function(w,d,s,g,js,fs){
   g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
@@ -8,103 +21,117 @@
 }(window,document,'script'));
 
 
-$(document).ready(()=> {
-    gapi.analytics.ready(function() {
+$(document).ready(() => {
+  gapi.analytics.ready(() => {
 
-
-    var CLIENT_ID = '456569075688-n6uo0irm3rf0pjticr9ntjir3qmfa9uh.apps.googleusercontent.com';
+    const CLIENT_ID = '456569075688-n6uo0irm3rf0pjticr9ntjir3qmfa9uh.apps.googleusercontent.com';
 
     gapi.analytics.auth.authorize({
+        // auth-container is dom element that hosts the sign-in button during a sessions first load. sign in button can also contain an event listener to do something     else as well
         container: 'embed-api-auth-container',
+        //client ID of our project from developers console (using Sarahs)
         clientid: CLIENT_ID,
     });
 
+    // return user info to console when they sign in... (name, email, profilePic)
+    gapi.analytics.auth.on('signIn', function() {
+      console.log(gapi.analytics.auth.getUserProfile());
+    });
+
+
+
+    /************************************************************** Graph */
     /**
-     * Store a set of common DataChart config options since they're shared by
-     * both of the charts we're about to make.
-     */
-    var commonConfig = {
-        query: {
-        metrics: 'ga:sessions',
-        dimensions: 'ga:date'
-        },
-        chart: {
-        type: 'LINE',
-        options: {
-            width: '100%'
-        }
-        }
-    };
-
-
-    /**
-     * Query params representing the first chart's date range.
-     */
-    var dateRange1 = {
-        'start-date': '14daysAgo',
-        'end-date': '8daysAgo'
-    };
-
-    /**
-     * Create a new ViewSelector2 instance to be rendered inside of an
-     * element with the id "view-selector-container".
-     */
-    var viewSelector = new gapi.analytics.ext.ViewSelector2({
-        container: 'view-selector-container',
-    }).execute();
-
-    
-        /**
      * Create a new DataChart instance with the given query parameters
      * and Google chart options. It will be rendered inside an element
      * with the id "chart-container".
      */
-    var dataChart1 = new gapi.analytics.googleCharts.DataChart(commonConfig)
-        .set({query: dateRange1})
-        .set({chart: {container: 'chart-container'}});
+    const commonConfig = {
+      query: {
+        metrics: 'ga:sessions',
+        dimensions: 'ga:date'
+      },
+      chart: {
+        type: 'COLUMN',
+        options: {
+            color: 'red',
+            legend: 'middle',
+            is3D: true,
+            width: '100%',
+            title: 'Sessions for the past 2 weeks',
+            fontSize: 16 // font size for pop-up window when hovering over a data plot from the graph
+        }
+      }
+    };
 
+    const dateRange1 = {
+      'start-date': '14daysAgo',
+      'end-date': '8daysAgo'
+    };
+
+    const dataChart1 = new gapi.analytics.googleCharts.DataChart(commonConfig)
+    .set(
+      {query: dateRange1}
+    )
+    .set(
+      {chart: {container: 'chart-container'}}
+    );
+
+
+    /************************************************************** Date Range */
     /**
-   * Create a new DateRangeSelector instance to be rendered inside of an
-   * element with the id "date-range-selector-1-container", set its date range
-   * and then render it to the page.
-   */
-    var dateRangeSelector1 = new gapi.analytics.ext.DateRangeSelector({
-        container: 'date-range-selector-container'
+     * Create a new DateRangeSelector instance to be rendered inside of an
+     * element with the id "date-range-selector-1-container", set its date range
+     * and then render it to the page.
+     */
+
+    const dateRangeSelector1 = new gapi.analytics.ext.DateRangeSelector({
+      container: 'date-range-selector-container'
     })
     .set(dateRange1)
     .execute();
 
-    /**
-     * Register a handler to run whenever the user changes the view.
-     * The handler will update both dataCharts as well as updating the title
-     * of the dashboard.
-     */
-    viewSelector.on('viewChange', function(data) {
-        dataChart1.set({query: {ids: data.ids}}).execute();
 
-        var title = document.getElementById('view-name');
-        title.textContent = data.property.name + ' (' + data.view.name + ')';
+    /************************************************************** View Selector */
+    /**
+     * Create a new ViewSelector2 instance to be rendered inside of an
+     * element with the id "view-selector-container".
+     */
+    const viewSelector = new gapi.analytics.ext.ViewSelector2({
+      container: 'view-selector-container'
+    })
+    .execute();
+
+    /************************************************************** Listener for Chart*/
+    dataChart1.on('success', (result) => {
+      console.log(result.data) // raw data of the graph values (x, y, and graph points)
+      console.log(result.chart) // gives info of chart.. can manipulate chart using js/jquery with this info ;)
+      console.log(result.response) // raw data of the entire response... ;)
+    })
+
+    dataChart1.on('error', (result) => {
+      console.log('Error occured during query or rendering')
+    })
+
+    /************************************************************** Listener for View*/
+    viewSelector.on('viewChange', (data) => {
+      // updates graph
+      dataChart1.set({query: {ids: data.ids}}).execute();
+
+      // updates title 
+      const title = document.getElementById('view-name');
+      title.textContent = data.property.name + ' (' + data.view.name + ')';
     });
 
+    /************************************************************** Listener for Date*/
+    dateRangeSelector1.on('change', (data) => {
+      // updates graph
+      dataChart1.set({query: data}).execute();
 
-    /**
-     * Register a handler to run whenever the user changes the date range from
-     * the first datepicker. The handler will update the first dataChart
-     * instance as well as change the dashboard subtitle to reflect the range.
-     */
-    dateRangeSelector1.on('change', function(data) {
-        dataChart1.set({query: data}).execute();
-
-        // Update the "from" dates text.
-        var datefield = document.getElementById('from-dates');
-        datefield.textContent = data['start-date'] + '&mdash;' + data['end-date'];
-        });
+      // Update the "from" dates text.
+      const datefield = document.getElementById('from-dates');
+      datefield.textContent = data['start-date'] + '&mdash;' + data['end-date'];
     });
 
+  });
 });
-
-    
-    
-
-
-
